565acfe702082db237ea2d02c262156d6bf4bd28
adwaithcd <adwaithgowda@gmail.com>
2018-10-24 18:04:27 +0530

    Modified code

--- Hospital/admin/addroom.php
+++ Hospital/admin/addroom.php
@@ -21,7 +21,7 @@ if (empty($_SESSION['admin']) OR empty($_SESSION['type'])) {
 			<a href="rooms.php" style="margin-left:10px;"><button class="btnlink">View Rooms</button></a><br>
 			<center>
 				<form action="addroom.php" method="POST">
-				<input type="number" name="number" class="form" placeholder="Enter Room Number" required="required"><br><br>
+				<input type="number" name="number" class="form" placeholder="Enter Room No" required="required"><br><br>
 				<input type="text" name="name" class="form" placeholder="Enter Room Name" required="required"><br><br>
 				<input type="submit" value="Add" class="btnlink" name="btn">
 			</form>
